a895d3b189353083785dfa65a6cb32ff6d981db5
UnBearAble1 <115948081+UnBearAble1@users.noreply.github.com>
2022-12-31 16:23:40 -0600

    Add files via upload

---
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,49 @@
+-- Join Employee table and Titles table to create retirments titles table
+SELECT  e.emp_no,
+        e.first_name,
+        e.last_name,
+        t.title,
+        t.from_date,
+        t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no
+
+DROP TABLE IF EXISTS unique_titles;
+
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Get count of retiring and group by title 
+SELECT COUNT (qt.title), qt.title
+INTO retiring_titles
+FROM unique_titles AS qt
+GROUP BY qt.title
+ORDER BY COUNT (qt.title) DESC;
+
+-- Find Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees AS e 
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
